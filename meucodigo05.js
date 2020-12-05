9a56ff90b41ca9be17916c8865377d20d6106bb2
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-12-05 07:49:18 -0300

    Códigos JavaScript

---
+++ meucodigo05.js
@@ -0,0 +1,8 @@
+var inicio = 2;
+var final = 1000;
+var todosPares = "Números Pares: ";
+while(inicio <= final){
+	todosPares = todosPares + inicio + ", ";
+	inicio = inicio + 2;
+}
+alert(todosPares);
