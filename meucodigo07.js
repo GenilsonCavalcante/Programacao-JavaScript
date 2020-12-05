39c641549470d1c208aebec45b9779bb49f9233d
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-12-05 07:49:44 -0300

    Códigos JavaScript

---
+++ meucodigo07.js
@@ -0,0 +1,8 @@
+function umClique(){
+	document.getElementById("imagem").src="Imagens/DeathNote.png";
+	document.getElementById("log").innerHTML += "troca para a foto do L<br>";
+}
+function doisCliques(){
+	document.getElementById("imagem").src="Imagens/download.jpeg";
+	document.getElementById("log").innerHTML += "troca para o logo do IFPB<br>";
+}
